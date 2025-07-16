7e6dc03139166f4f978531245b091a1c57d21997
Adam <73994535+adamtuft@users.noreply.github.com>
2025-07-16 13:29:01 +0100

    refactor: Remove suffix from variants

--- cargo-fetch-source-lib/src/error.rs
+++ cargo-fetch-source-lib/src/error.rs
@@ -1,17 +1,15 @@
-// use crate::git2_ext;
+
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error(transparent)]
-    IoError(#[from] std::io::Error),
+    Io(#[from] std::io::Error),
     #[error(transparent)]
-    ReqwestError(#[from] reqwest::Error),
+    Reqwest(#[from] reqwest::Error),
     #[error(transparent)]
-    SerdeJsonError(#[from] serde_json::Error),
+    SerdeJson(#[from] serde_json::Error),
     #[error(transparent)]
-    UrlParseError(#[from] url::ParseError),
-    #[error("Manual error: {0}")]
-    Manual(String),
+    UrlParse(#[from] url::ParseError),
     #[error("Command '{command}' exited with status {status}\n{stderr}")]
     Subprocess {
         command: String,
